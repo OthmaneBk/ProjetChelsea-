4f1f49eb4d519db20cb2cfd921d376d2eed9c8c7
OthmaneBk <okebennani@gmail.com>
2025-05-04 19:42:58 +0100

    updatePath

--- workflowData/collecte.py
+++ workflowData/collecte.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
 def collecte_data():
-   data_gps=pd.read_csv('D://Othmane//4eme//ChelaseProjet//GpsData//data//gpsD1.csv',encoding='latin-1',sep=';')
+   data_path = 'data/gpsD1.csv'
+   data_gps=pd.read_csv(data_path,encoding='latin-1',sep=';')
    return data_gps
