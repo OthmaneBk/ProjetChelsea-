92fc6445b55f4fa1880883052ac3764d83e1ba64
OthmaneBk <okebennani@gmail.com>
2025-05-06 12:51:31 +0100

    use moden.pandas

--- workflowData/Data3/collecte.py
+++ workflowData/Data3/collecte.py
@@ -1,6 +1,6 @@
-import modin.pandas as pd
+import pandas as pd
 
 def collecte_data():
-   data_path = 'data/BioPlayer.csv'
+   data_path = 'data/Individual_Priority.csv'
    data_gps=pd.read_csv(data_path,encoding='latin-1',sep=';')
    return data_gps
